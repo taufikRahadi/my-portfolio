6ce205024485a7f2346fb2b161fc0459d97d1e64
unknown <taufikrahadi1@gmail.com>
2020-01-14 22:47:51 +0700

    Updates

---
+++ precache-manifest.05bbd5aaf07da78be5daeef72376fac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889ae9774f9d89f57a9ac3c38322edad",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0619ebf761417b8206cf",
+    "url": "/my-portfolio/static/css/main.74fdb1f7.chunk.css"
+  },
+  {
+    "revision": "fc461e4a67800bc54a84",
+    "url": "/my-portfolio/static/js/2.0abf5e3f.chunk.js"
+  },
+  {
+    "revision": "0619ebf761417b8206cf",
+    "url": "/my-portfolio/static/js/main.8eebc253.chunk.js"
+  },
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  }
+]);
