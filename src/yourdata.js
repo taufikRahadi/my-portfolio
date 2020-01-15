e0456e74658095f329616c99797c733f4fbf94c9
unknown <taufikrahadi1@gmail.com>
2020-01-15 20:24:46 +0700

    last

--- src/yourdata.js
+++ src/yourdata.js
@@ -8,7 +8,6 @@ export default
 
                         'from Indonesia'
     ],
-        //Contact Email
         contactEmail:'taufikrahadi1@gmail.com',
         abouttext: "Hello, My name is Taufik, 19 years old. Based in Pekanbaru, Riau, Indonesia. I like to gain new experience and learn new things.",
         aboutImage:"https://previews.dropbox.com/p/thumb/AAos4ZOB4Wuy81vxFmJAquKYJO7blkcEjKclCdh7lxqxOSUrvAv814iG78cDYk4C23fX_XMghbyvROT_ZncA13hvQh9pXv0UVJe0QuXrmssspmETl0WUDmVLs7kgAoqbx6wK8tFQxtx20u_DfQ2muYA_GYtSkHUxqAZ45zrICA9dLJ0yfYjC-Rgp2yqjRouR1pjQGUvwJOcm042T8sJijIsoSGbwcphYP1vW3TmGpmEhjTJKVmv0DaO09vG1KBO6se4F8SCY883MLUaTzFzxXU_0IDPcc97wv9ryk6sWBxKjkcWR3w0M4KyeYwJPJv-DbyhbryZMB3o1T6SmFgmSYgCwDDP9yM8_rh8JXSFCvR0ZqQ/p.jpeg?fv_content=true&size_mode=5",
@@ -16,32 +15,30 @@ export default
        projects:[
            {
             id: 1,
-            title:'Karma Shop', //Project Title - Add Your Project Title Here
-             service:'Website Development', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc: "https://previews.dropbox.com/p/thumb/AArBL7xZaodtvQhshEupkBKXx_8cFSH7cYQwrqK_BNjT2qZpI7useW5VxR1IaaVYq3yzuBjCume2Y3-OXhWwEAIUbpa5QObCdiiI-xqlrNgHCBdlnjzVa4rkxwviRsTZaDTCDK4Df0zTI2RRgx94DuidnciIEFTNDelWfQrUIrHagf5N4sCOewmiwVoXTLBQhChDX_A2JREbU7F2KWaHu7eidJrvEss0PbD5n8Jeb2FPNqFZxXRCOLJxz_hT6JtBcb7LJwSG7xppHDpGQ4a9hy1rDaYxOW3S_mzF6sWupALe5bmMgMV4PIUhMbWHP7H1B8-6v1JQ8uW6cHI1ybp_fhkI/p.png?fv_content=true&size_mode=5",
-             //Project URL - Add Your Project Url Here
+            title:'Karma Shop',
+             service:'Website Development',
+             imageSrc: "https://i.ibb.co/d6xQFg1/Screenshot-93.png",
              url:'https://github.com/taufikRahadi'
             },
             {
                 id: 2,
                 title: 'Keuangan Masjid',
                 service: 'Website Development',
-                imageSrc: "https://previews.dropbox.com/p/thumb/AAq3nRwVUSTX6SjnyG1hr41nI2sFraQdZrOp0_EszRMgimp08VXU4vc6_IJbnFR8XKCX7OEufNc49XCjD6jpBdJse5aq0R6gZmS43dRubmP8oH99IApUdYeoeBUQ8-HaG2ecO0NO_t4yx-90lVeT9aU1AjuPlRFHqo_PSimozFmTuldGCP-_QpvRZfON3wO8OKS3kqqTPBE-KsP0-JRZdw9569HWIT5pYinnDsiIwmnCpm6CPgmp9POjrsYPqrzl8Ddsw_NuX4Lq_IMLynoWH8LjcFPdLseGSEc-v8x6ezY8XdewsyYlIXKmsPhqAi2BWLumapAe52I4qJIVQkCXAQb-/p.png?fv_content=true&size_mode=5",
+                imageSrc: "https://i.ibb.co/DG89TXH/Screenshot-29.png",
                 url: 'https://github.com/taufikRahadi'
             },
             {
                 id: 3,
                 title: 'Mockup',
                 service: 'Web Designing',
-                imageSrc: "https://previews.dropbox.com/p/thumb/AArjpvHMTEo5DaTp841BTqbo6ZakUizFrv1JEvw3gPPz-AVY-DNOaNF6B-gSgoyNtr47N8xORJGOWU-Zv89wKaO89Ik9ZiBkjddfxWJBHtINMI3T5UxI9JQ_iYeA9eUMWIdMdnco3g-aMeYXmSLbsqWIsspUZ63TAd-0V87SJf-nwU60WcVpdIzoIVjZtko6KeB_OiNLAEkDe8k_QkHQMn7s-AmC-83jGIU7jLevcekW8tUzpoWlcWQnl7chkcRFKz0nORpqBiUH54tPmsheRCB1nuFdnoM6bp6_RZC58DXwjXIQQ2OqnQ2RNxPz3ciWparckTQ7xJFC3cdCzgSHnsZv/p.jpeg?size=1280x960&size_mode=3",
+                imageSrc: "https://i.ibb.co/nQxTX1Y/dreamit.jpg",
                 url: 'https://github.com/taufikRahadi'
             },
             {
                 id: 4,
                 title: 'Mockup',
                 service: 'Web Designing',
-                imageSrc: "https://previews.dropbox.com/p/thumb/AAqlqx3nkVFVDvRzECjot4BJjoJXAlyyW0ntIbySJ7iXoaGuoMPJbPAExwJBbqRdXIfiLZmjpLGoik-NaZ3CheBNgiwWMetj2_jL6qpIzddtlUTmd_o0mGvz9oIpDeeaqlHFpAhg4fc_Logzio_ByoAcMFk4ylZLTwNYzwi8Ud8BNEf4-YhldECTEmvwLfrNKzW2DXfpUR61jcOgPuKK53T7oDXsGfdEtgu5ZCaWsOau4Osvj6aogT8qv2xqGrVhW47MB3ZZyEaQ6FdeE3Dd5HcLJbUBKixbIWTrPYtTGYPWZU24u3ibupl6_3Q1piXhZNVPs2lJl70W0dEb6CLwHCD9/p.jpeg?size=1600x1200&size_mode=3",
+                imageSrc: "https://i.ibb.co/x2B93zy/MONEYTIZE.jpg",
                 url: 'https://github.com/taufikRahadi'
             }
         ],
